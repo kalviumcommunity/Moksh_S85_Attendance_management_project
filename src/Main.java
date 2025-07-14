17c54bab6b92fb431bb325460b77573a629b5f42
moksh59022 <vsmokshsharma688@gmail.com>
2025-07-14 09:43:14 +0530

    part-01

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the School Attendance System Project!");
+        System.out.println("Session 1: Project Setup and Orientation Complete.");
+    }
+}
